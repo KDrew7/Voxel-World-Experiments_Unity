43116cad80bd9d50105ef0bd244b312e1dfcf29d
KDrew7 <kdrew7@live.com>
2022-05-30 15:28:52 -0700

    Initial commit

---
+++ Assets/Block.cs
@@ -0,0 +1,394 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block {
+
+	enum Cubeside {BOTTOM, TOP, LEFT, RIGHT, FRONT, BACK, BOTTOML,BOTTOMR,TOPL,TOPR,FRONTL,FRONTR,BACKL,BACKR};
+	public enum BlockType {GRASS, DIRT, STONE, BEDROCK, REDSTONE, DIAMOND, AIR, SPLIT};
+
+	public BlockType bType;
+	public bool isSolid;
+	Chunk owner;
+	GameObject parent;
+	Vector3 position;
+
+	Vector2[,] blockUVs = { 
+		/*GRASS TOP*/		{new Vector2( 0.125f, 0.375f ), new Vector2( 0.1875f, 0.375f),
+								new Vector2( 0.125f, 0.4375f ),new Vector2( 0.1875f, 0.4375f )},
+		/*GRASS SIDE*/		{new Vector2( 0.1875f, 0.9375f ), new Vector2( 0.25f, 0.9375f),
+								new Vector2( 0.1875f, 1.0f ),new Vector2( 0.25f, 1.0f )},
+		/*DIRT*/			{new Vector2( 0.125f, 0.9375f ), new Vector2( 0.1875f, 0.9375f),
+								new Vector2( 0.125f, 1.0f ),new Vector2( 0.1875f, 1.0f )},
+		/*STONE*/			{new Vector2( 0, 0.875f ), new Vector2( 0.0625f, 0.875f),
+								new Vector2( 0, 0.9375f ),new Vector2( 0.0625f, 0.9375f )},
+		/*BEDROCK*/			{new Vector2( 0.3125f, 0.8125f ), new Vector2( 0.375f, 0.8125f),
+								new Vector2( 0.3125f, 0.875f ), new Vector2( 0.375f, 0.875f )},
+		/*REDSTONE*/		{new Vector2( 0.1875f, 0.75f ), new Vector2( 0.25f, 0.75f),
+								new Vector2( 0.1875f, 0.8125f ),new Vector2( 0.25f, 0.8125f )},
+		/*DIAMOND*/			{new Vector2( 0.125f, 0.75f ), new Vector2( 0.1875f, 0.75f),
+								new Vector2( 0.125f, 0.8125f ),new Vector2( 0.1875f, 0.8125f )}
+						}; 
+
+
+	public Block(BlockType b, Vector3 pos, GameObject p, Chunk o)
+	{
+		bType = b;
+		owner = o;
+		parent = p;
+		position = pos;
+		if(bType == BlockType.AIR)
+			isSolid = false;
+		else
+			isSolid = true;
+	}
+
+	public Block(BlockType b, Vector3 pos, GameObject p)
+	{
+		bType = b;
+		parent = p;
+		position = pos;
+		if(bType == BlockType.AIR)
+			isSolid = false;
+		else
+			isSolid = true;
+	}
+
+	public void SetType(BlockType b)
+	{
+		bType = b;
+		if(bType == BlockType.AIR)
+			isSolid = false;
+		else
+			isSolid = true;
+	}
+
+
+	void CreateQuad(Cubeside side)
+	{
+		Mesh mesh = new Mesh();
+	    mesh.name = "ScriptedMesh" + side.ToString(); 
+
+		if (bType == BlockType.REDSTONE) {
+		Vector3[] vertices = new Vector3[4];
+		Vector3[] normals = new Vector3[4];
+		Vector2[] uvs = new Vector2[4];
+		int[] triangles = new int[6];
+
+		//all possible UVs
+		
+
+		
+		Vector2 uv00 = blockUVs[(int)(bType+1),0];
+		Vector2 uv10 = blockUVs[(int)(bType+1),1];
+		Vector2 uv01 = blockUVs[(int)(bType+1),2];
+		Vector2 uv11 = blockUVs[(int)(bType+1),3];
+		Vector2 vv00 = blockUVs[(int)(BlockType.STONE+1),0];
+		Vector2 vv10 = blockUVs[(int)(BlockType.STONE+1),1];
+		Vector2 vv01 = blockUVs[(int)(BlockType.STONE+1),2];
+		Vector2 vv11 = blockUVs[(int)(BlockType.STONE+1),3];
+		
+
+		//all possible vertices 
+		Vector3 p0 = new Vector3( -0.5f,  -0.5f,  0.5f );
+		Vector3 p1 = new Vector3(  0.5f,  -0.5f,  0.5f );
+		Vector3 p2 = new Vector3(  0.5f,  -0.5f, -0.5f );
+		Vector3 p3 = new Vector3( -0.5f,  -0.5f, -0.5f );		 
+		Vector3 p4 = new Vector3( -0.5f,   0.5f,  0.5f );
+		Vector3 p5 = new Vector3(  0.5f,   0.5f,  0.5f );
+		Vector3 p6 = new Vector3(  0.5f,   0.5f, -0.5f );
+		Vector3 p7 = new Vector3( -0.5f,   0.5f, -0.5f );
+
+		Vector3 q0 = new Vector3( 0.0f, -0.5f, -0.5f);
+		Vector3 q1 = new Vector3( 0.0f, -0.5f,  0.5f);
+		Vector3 q2 = new Vector3( 0.0f,  0.5f, -0.5f);
+		Vector3 q3 = new Vector3( 0.0f,  0.5f,  0.5f);
+
+
+		switch(side)
+		{
+			case Cubeside.BOTTOML:
+				vertices = new Vector3[] {p0, q1, q2, p3};
+				normals = new Vector3[] {Vector3.down, Vector3.down, 
+											Vector3.down, Vector3.down};
+				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
+				triangles = new int[] { 3, 1, 0, 3, 2, 1};
+			break;
+			case Cubeside.BOTTOMR:
+				vertices = new Vector3[] {p0, p1, p2, p3};
+				normals = new Vector3[] {Vector3.down, Vector3.down, 
+											Vector3.down, Vector3.down};
+				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
+				triangles = new int[] { 3, 1, 0, 3, 2, 1};
+			break;
+			case Cubeside.TOPL:
+				vertices = new Vector3[] {p7, q2, q3, p4};
+				normals = new Vector3[] {Vector3.up, Vector3.up, 
+											Vector3.up, Vector3.up};
+				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
+				triangles = new int[] {3, 1, 0, 3, 2, 1};
+			break;
+			case Cubeside.TOPR:
+				vertices = new Vector3[] {q2, p6, p5, q3};
+				normals = new Vector3[] {Vector3.up, Vector3.up, 
+											Vector3.up, Vector3.up};
+				uvs = new Vector2[] {vv11, vv01, vv00, vv10};
+				triangles = new int[] {3, 1, 0, 3, 2, 1};
+			break;
+			case Cubeside.LEFT:
+				vertices = new Vector3[] {p7, p4, p0, p3};
+				normals = new Vector3[] {Vector3.left, Vector3.left, 
+											Vector3.left, Vector3.left};
+				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
+				triangles = new int[] {3, 1, 0, 3, 2, 1};
+			break;
+			case Cubeside.RIGHT:
+				vertices = new Vector3[] {p5, p6, p2, p1};
+				normals = new Vector3[] {Vector3.right, Vector3.right, 
+											Vector3.right, Vector3.right};
+				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
+				triangles = new int[] {3, 1, 0, 3, 2, 1};
+			break;
+			case Cubeside.FRONTL:
+				vertices = new Vector3[] {p4, p5, p1, p0};
+				normals = new Vector3[] {Vector3.forward, Vector3.forward, 
+											Vector3.forward, Vector3.forward};
+				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
+				triangles = new int[] {3, 1, 0, 3, 2, 1};
+			break;
+			case Cubeside.FRONTR:
+				vertices = new Vector3[] {p4, p5, p1, p0};
+				normals = new Vector3[] {Vector3.forward, Vector3.forward, 
+											Vector3.forward, Vector3.forward};
+				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
+				triangles = new int[] {3, 1, 0, 3, 2, 1};
+			break;
+			case Cubeside.BACKL:
+				vertices = new Vector3[] {p6, p7, p3, p2};
+				normals = new Vector3[] {Vector3.back, Vector3.back, 
+											Vector3.back, Vector3.back};
+				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
+				triangles = new int[] {3, 1, 0, 3, 2, 1};
+			break;
+			case Cubeside.BACKR:
+				vertices = new Vector3[] {p6, p7, p3, p2};
+				normals = new Vector3[] {Vector3.back, Vector3.back, 
+											Vector3.back, Vector3.back};
+				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
+				triangles = new int[] {3, 1, 0, 3, 2, 1};
+			break;
+		}
+		mesh.vertices = vertices;
+		mesh.normals = normals;
+		mesh.uv = uvs;
+		mesh.triangles = triangles;
+		}
+
+
+
+
+
+
+		else {
+
+		Vector3[] vertices = new Vector3[4];
+		Vector3[] normals = new Vector3[4];
+		Vector2[] uvs = new Vector2[4];
+		int[] triangles = new int[6];
+
+		//all possible UVs
+		Vector2 uv00;
+		Vector2 uv10;
+		Vector2 uv01;
+		Vector2 uv11;
+
+		if(bType == BlockType.GRASS && side == Cubeside.TOP)
+		{
+			uv00 = blockUVs[0,0];
+			uv10 = blockUVs[0,1];
+			uv01 = blockUVs[0,2];
+			uv11 = blockUVs[0,3];
+		}
+		else if(bType == BlockType.GRASS && side == Cubeside.BOTTOM)
+		{
+			uv00 = blockUVs[(int)(BlockType.DIRT+1),0];
+			uv10 = blockUVs[(int)(BlockType.DIRT+1),1];
+			uv01 = blockUVs[(int)(BlockType.DIRT+1),2];
+			uv11 = blockUVs[(int)(BlockType.DIRT+1),3];
+		}
+
+		else
+		{
+			uv00 = blockUVs[(int)(bType+1),0];
+			uv10 = blockUVs[(int)(bType+1),1];
+			uv01 = blockUVs[(int)(bType+1),2];
+			uv11 = blockUVs[(int)(bType+1),3];
+		}
+
+		//all possible vertices 
+		Vector3 p0 = new Vector3( -0.5f,  -0.5f,  0.5f );
+		Vector3 p1 = new Vector3(  0.5f,  -0.5f,  0.5f );
+		Vector3 p2 = new Vector3(  0.5f,  -0.5f, -0.5f );
+		Vector3 p3 = new Vector3( -0.5f,  -0.5f, -0.5f );		 
+		Vector3 p4 = new Vector3( -0.5f,   0.5f,  0.5f );
+		Vector3 p5 = new Vector3(  0.5f,   0.5f,  0.5f );
+		Vector3 p6 = new Vector3(  0.5f,   0.5f, -0.5f );
+		Vector3 p7 = new Vector3( -0.5f,   0.5f, -0.5f );
+
+		Vector3 q0 = new Vector3( 0.0f, -0.5f, -0.5f);
+		Vector3 q1 = new Vector3( 0.0f, -0.5f, 0.5f);
+		Vector3 q2 = new Vector3( 0.0f, 0.5f, -0.5f);
+		Vector3 q3 = new Vector3( 0.0f, 0.5f, 0.5f);
+
+
+		switch(side)
+		{
+			case Cubeside.BOTTOM:
+				vertices = new Vector3[] {p0, p1, p2, p3};
+				normals = new Vector3[] {Vector3.down, Vector3.down, 
+											Vector3.down, Vector3.down};
+				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
+				triangles = new int[] { 3, 1, 0, 3, 2, 1};
+			break;
+			case Cubeside.TOP:
+				vertices = new Vector3[] {p7, p6, p5, p4};
+				normals = new Vector3[] {Vector3.up, Vector3.up, 
+											Vector3.up, Vector3.up};
+				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
+				triangles = new int[] {3, 1, 0, 3, 2, 1};
+			break;
+			case Cubeside.LEFT:
+				vertices = new Vector3[] {p7, p4, p0, p3};
+				normals = new Vector3[] {Vector3.left, Vector3.left, 
+											Vector3.left, Vector3.left};
+				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
+				triangles = new int[] {3, 1, 0, 3, 2, 1};
+			break;
+			case Cubeside.RIGHT:
+				vertices = new Vector3[] {p5, p6, p2, p1};
+				normals = new Vector3[] {Vector3.right, Vector3.right, 
+											Vector3.right, Vector3.right};
+				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
+				triangles = new int[] {3, 1, 0, 3, 2, 1};
+			break;
+			case Cubeside.FRONT:
+				vertices = new Vector3[] {p4, p5, p1, p0};
+				normals = new Vector3[] {Vector3.forward, Vector3.forward, 
+											Vector3.forward, Vector3.forward};
+				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
+				triangles = new int[] {3, 1, 0, 3, 2, 1};
+			break;
+			case Cubeside.BACK:
+				vertices = new Vector3[] {p6, p7, p3, p2};
+				normals = new Vector3[] {Vector3.back, Vector3.back, 
+											Vector3.back, Vector3.back};
+				uvs = new Vector2[] {uv11, uv01, uv00, uv10};
+				triangles = new int[] {3, 1, 0, 3, 2, 1};
+			break;
+		}
+
+		mesh.vertices = vertices;
+		mesh.normals = normals;
+		mesh.uv = uvs;
+		mesh.triangles = triangles;
+		}
+		
+		 
+		mesh.RecalculateBounds();
+		
+		GameObject quad = new GameObject("Quad");
+		quad.transform.position = position;
+	    quad.transform.parent = this.parent.transform;
+
+     	MeshFilter meshFilter = (MeshFilter) quad.AddComponent(typeof(MeshFilter));
+		meshFilter.mesh = mesh;
+	}
+
+	int ConvertBlockIndexToLocal(int i)
+	{
+		if(i == -1) 
+			i = World.chunkSize-1; 
+		else if(i == World.chunkSize) 
+			i = 0;
+		return i;
+	}
+
+	public bool HasSolidNeighbour(int x, int y, int z)
+	{
+		Block[,,] chunks;
+
+		if(x < 0 || x >= World.chunkSize || 
+		   y < 0 || y >= World.chunkSize ||
+		   z < 0 || z >= World.chunkSize)
+		{  //block in a neighbouring chunk
+			
+			Vector3 neighbourChunkPos = this.parent.transform.position + 
+										new Vector3((x - (int)position.x)*World.chunkSize, 
+											(y - (int)position.y)*World.chunkSize, 
+											(z - (int)position.z)*World.chunkSize);
+			string nName = World.BuildChunkName(neighbourChunkPos);
+
+			x = ConvertBlockIndexToLocal(x);
+			y = ConvertBlockIndexToLocal(y);
+			z = ConvertBlockIndexToLocal(z);
+			
+			Chunk nChunk;
+			if(World.chunks.TryGetValue(nName, out nChunk))
+			{
+				chunks = nChunk.chunkData;
+			}
+			else
+				return false;
+		}  //block in this chunk
+		else
+			chunks = owner.chunkData;
+		
+		try
+		{
+			return chunks[x,y,z].isSolid;
+		}
+		catch(System.IndexOutOfRangeException){}
+
+		return false;
+	}
+
+	public void Draw()
+	{
+		if(bType == BlockType.REDSTONE) {
+			if(!HasSolidNeighbour((int)position.x,(int)position.y,(int)position.z + 1))
+				CreateQuad(Cubeside.FRONTL);
+				CreateQuad(Cubeside.FRONTR);
+			if(!HasSolidNeighbour((int)position.x,(int)position.y,(int)position.z - 1))
+				CreateQuad(Cubeside.BACKL);
+				CreateQuad(Cubeside.BACKR);
+			if(!HasSolidNeighbour((int)position.x,(int)position.y + 1,(int)position.z))
+				CreateQuad(Cubeside.TOPL);
+				CreateQuad(Cubeside.TOPR);
+			if(!HasSolidNeighbour((int)position.x,(int)position.y - 1,(int)position.z))
+				CreateQuad(Cubeside.BOTTOML);
+				CreateQuad(Cubeside.BOTTOMR);
+			if(!HasSolidNeighbour((int)position.x - 1,(int)position.y,(int)position.z))
+				CreateQuad(Cubeside.LEFT);
+			if(!HasSolidNeighbour((int)position.x + 1,(int)position.y,(int)position.z))
+				CreateQuad(Cubeside.RIGHT);
+		}
+		
+		else {
+
+		if(bType == BlockType.AIR) return;
+
+		if(!HasSolidNeighbour((int)position.x,(int)position.y,(int)position.z + 1))
+			CreateQuad(Cubeside.FRONT);
+		if(!HasSolidNeighbour((int)position.x,(int)position.y,(int)position.z - 1))
+			CreateQuad(Cubeside.BACK);
+		if(!HasSolidNeighbour((int)position.x,(int)position.y + 1,(int)position.z))
+			CreateQuad(Cubeside.TOP);
+		if(!HasSolidNeighbour((int)position.x,(int)position.y - 1,(int)position.z))
+			CreateQuad(Cubeside.BOTTOM);
+		if(!HasSolidNeighbour((int)position.x - 1,(int)position.y,(int)position.z))
+			CreateQuad(Cubeside.LEFT);
+		if(!HasSolidNeighbour((int)position.x + 1,(int)position.y,(int)position.z))
+			CreateQuad(Cubeside.RIGHT);
+		}
+	}
+}
---
+++ Assets/GraphNoise.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GraphNoise : MonoBehaviour {
+
+	float t = 0;
+    float inc = 0.01f;
+    public static int maxHeight = 150;
+    static float smooth = 0.003f;
+    static int octaves = 4;
+    static float persistence = 0.8f;
+
+	static float Map(float min, float max, float omin, float omax, float value)
+    {
+        return Mathf.Lerp (min, max, Mathf.InverseLerp (omin, omax, value));
+    }
+
+    public static int GenHeight(float x, float z)
+    {
+        float height = Map(0, maxHeight, 0, 1, FBM(x * smooth, z * smooth, octaves, persistence));
+        return (int)height;
+    }
+
+    public static int GenHeightStone(float x, float z)
+    {
+        float height = Map(0, maxHeight-10, 0, 1, FBM(x * smooth*3, z * smooth*3, octaves+3, persistence));
+        return (int)height;
+    }
+
+    public static int GenHeightGrass(float x, float z)
+    {
+        float height = Map(0, maxHeight+1, 0, 1, FBM(x * smooth, z * smooth, octaves, persistence));
+        return (int)height;
+    }
+
+
+
+    void Update () 
+	{
+        t += inc;
+        //float n = FBM(t,6,0.8f);
+        //Grapher.Log(n, "Perlin1", Color.red);  
+	}
+
+	static float FBM(float x, float z, int octaves, float persistence)
+    {
+        float total = 0;
+        float frequency = 1;
+        float amplitude = 1;
+        float maxValue = 0; 
+        for(int i = 0; i < octaves; i++) 
+        {
+                total += Mathf.PerlinNoise(x * frequency, z * frequency) * amplitude;
+                maxValue += amplitude;
+                amplitude *= persistence;
+                frequency *= 2;
+        }
+
+        return total/maxValue;
+    }
+
+    public static float FBM3D(float x, float y, float z, int oct, float pers, float smoothCaves)
+    {   
+        
+        float XY = FBM(x * smoothCaves, y * smoothCaves, oct, 0.5f);
+        float YZ = FBM(y * smoothCaves, z * smoothCaves, oct, 0.5f);
+        float XZ = FBM(x * smoothCaves, z * smoothCaves, oct, 0.5f);
+
+        float YX = FBM(y * smoothCaves, x * smoothCaves, oct, 0.5f);
+        float ZY = FBM(z * smoothCaves, y * smoothCaves, oct, 0.5f);
+        float ZX = FBM(z * smoothCaves, x * smoothCaves, oct, 0.5f);
+
+        return (XY + YZ + XZ + YX + ZY + ZX) / 6.0f;
+    }
+}
